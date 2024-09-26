62f0a963e6c007f715b5cd873095c41ad0085890
kshittiz101 <chaudharykshittiz950@gmail.com>
2024-09-26 21:30:49 +0545

    Array in js

--- 02_basic/03_Array/array_01.js
+++ 02_basic/03_Array/array_01.js
@@ -8,10 +8,30 @@
 
 // --------------------------------------------------------------------------------------------------------
 //1. using array literals
-let numArray = [1, 2, 3, 4, 5];
+// let numArray = [1, 2, 3, 4, 5];
 
 //2. using Array constructor
-let myArray = Array(10, 20, 30, 40, 50);
-for (let i = 0; i < myArray.length; i++) {
-  console.log(myArray[i]);
+// let myArray = Array(10, 20, 30, 40, 50);
+// for (let i = 0; i < myArray.length; i++) {
+//   console.log(myArray[i]);
+// }
+
+// 3 create an array with an Array.of() and Array.from()
+let arrayOfFruits = Array.of("orange", "apple", "banana", "papaya");
+for (fruit in arrayOfFruits) {
+  console.log(fruit);
 }
+// output will be indexes of the array variable not a value
+// because, The for...in loop in JavaScript is designed to iterate over
+// the enumerable properties of an object. When used on an array, it iterates over the array indices (keys),
+// which are the positions of the elements in the array (0, 1, 2, 3 in this case).
+
+// corrected code
+// let arrayOfFruits = Array.of("orange", "apple", "banana", "papaya");
+for (let fruit of arrayOfFruits) {
+  console.log(fruit);
+}
+
+// Note:
+// for...in Loop: Iterates over the enumerable properties (keys) of an object.
+// for...of Loop: Iterates over the values of an iterable object like an array.
