e456093426402e9ced47f1b496e8362d5c93c280
kshittiz101 <chaudharykshittiz950@gmail.com>
2024-10-19 07:56:14 +0545

    cat project with api calling

--- 07_advance/async/async/await/02_fetching_api.js
+++ 07_advance/async/async/await/02_fetching_api.js
@@ -1,32 +1,33 @@
-const URL = "https://api.thecatapi.com/v1/images/search?limit=10";
-let content = document.querySelector(".content");
+const URL = 'https://api.thecatapi.com/v1/images/search?limit=10';
+let content = document.querySelector('.content');
 
 const getCat = async () => {
-  console.log("getting data");
+  console.log('getting data');
 
   // Fetch the data from the API
   let response = await fetch(URL);
+  console.log(response);
   let data = await response.json();
 
   console.log(data); // Logs the array of cat objects
 
   // Clear the content before adding new images
-  content.innerHTML = "";
+  content.innerHTML = '';
 
   // Iterate through the data array
   for (let i = 0; i < data.length; i++) {
     // Create a container for each cat image
-    let container = document.createElement("div");
-    container.classList.add("image__container");
+    let container = document.createElement('div');
+    container.classList.add('image__container');
 
     // Create an img element for the cat image
-    let img = document.createElement("img");
-    img.setAttribute("src", data[i].url);
-    img.setAttribute("alt", `Cat Image ${i + 1}`);
+    let img = document.createElement('img');
+    img.setAttribute('src', data[i].url);
+    img.setAttribute('alt', `Cat Image ${i + 1}`);
 
     // Create a p element for the cat ID
-    let id = document.createElement("p");
-    id.classList.add("title");
+    let id = document.createElement('p');
+    id.classList.add('title');
     id.textContent = `Cat ID: ${data[i].id}`;
 
     // Append the img and id to the container
