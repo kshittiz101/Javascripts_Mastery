f75f577746655ffa4c0e6907e686faf75a9a8939
kshittiz101 <chaudharykshittiz950@gmail.com>
2025-03-25 08:26:22 +0545

    js function

--- 01-level/06-function/fun-01.js
+++ 01-level/06-function/fun-01.js
@@ -5,10 +5,8 @@
 // syntax to create function
 /* 
 function functionName(parameters){
-
         // function body
-
-return  (optional return body)
+        return  (optional return body)
 }
 
 */
@@ -22,3 +20,56 @@ function sum() {
 }
 // function calling
 sum();
+
+// types of function
+// 1.Built-in function
+// example: console.log(), window.alert(), console.table()
+
+// 2.user defined function
+// 2.1. Function with no parameters and no return types
+// 2.2. Function with  parameters and no return types
+// 2.3. Function with no parameters and  return types
+// 2.4. Function with  parameters and  return types
+
+// 2.1. Function with no parameters and no return types
+
+function message() {
+  console.log("Hello from kshittiz");
+}
+message();
+
+// 2.2. Function with  parameters and no return types
+function add(a, b) {
+  console.log(a + b);
+}
+
+add(20, 30);
+
+// 2.3. Function with no parameters and  return types
+
+function returnTypes() {
+  return "My name is kshittiz";
+}
+
+let myIntro = returnTypes();
+console.log(myIntro);
+
+// 2.4 Function with parameters and return types
+function substract(a, b) {
+  return a - b;
+}
+
+let result = substract(30, 20);
+console.log(result);
+
+// Notes:
+// A function stops executing once it hits return.
+// anythings return after the return keywords the code will not execute
+
+// Default parameters
+function defParameter(schoolName = "Amar Jyoti") {
+  console.log(schoolName);
+}
+
+defParameter();
+defParameter("Sainika");
---
+++ 01-level/06-function/fun-02.js
@@ -0,0 +1,16 @@
+// Function Expressions
+// A function expression is a function that is assigned to a variable. Unlike function declarations, function expressions are not hoisted, meaning they cannot be used before they are defined.
+
+/* 
+const functionName = function(parameters) {
+    // Function body
+    return value; // Optional
+};
+
+*/
+
+const sum = function (a, b) {
+  console.log(a + b);
+};
+
+sum(20, 85);
