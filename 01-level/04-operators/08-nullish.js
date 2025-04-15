86aac30f936e9fbd570a874a30061dea8b34148e
kshittiz101 <chaudharykshittiz950@gmail.com>
2025-04-15 12:23:15 +0545

    nullish coalescing operators

--- 01-level/04-operators/08-nullish.js
+++ 01-level/04-operators/08-nullish.js
@@ -1,9 +1,36 @@
-// Nullish Coalescing Operator (??)
-let userName = null;
-let displayName = userName ?? "Guest";
-console.log(displayName); // "Guest"
+// nullish Coalescing Operators (??)
+// it is a logical operator, introduce in Es2020
+// Returns the right hand operand only if the left hand operand in null or undefined
+// syntax:
+// leftOperand ?? rightOperand
 
-let userAge = 0;
-let finalAge = userAge ?? 18;
-console.log(finalAge);
-// 0 (Unlike `||`, `??` treats 0 as a valid value)
+console.log(null ?? "default");
+// 'default'
+// output will be default beacuse left hand operand in null
+
+console.log(undefined ?? "fallback");
+// 'fallback'
+// output it will show fallback beacuse left hand operand in undefined
+
+console.log(0 ?? 24);
+// 0
+// output will be 0 because the value of left hand operand in not null or undefined
+
+console.log(false ?? true);
+// false
+
+// logical OR (||) operator
+// this operator skips falsy values
+// some falsy values
+// false, 0 (zero), -0 (neg Zero), 0n (BigInt Zero), "" (Empty string), null , undefined, NaN (Not a Number)
+
+console.log(0 || "42");
+// output: '42'
+// beacuse left oprand in falsy values
+console.log(-0 || "hello");
+// output: 'hello'
+
+console.log(null || "somthing is here");
+
+console.log("with data" || "no data");
+// with data will output becasue it is not a falsy value
