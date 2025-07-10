b6c119fafdf03c97b9e9ade05b5f56256da1a8d7
kshittiz101 <chaudharykshittiz950@gmail.com>
2025-07-10 21:30:15 +0545

    different ways to add event in html

--- intermediate/DOM/lessons/05.event-handling/scripts.js
+++ intermediate/DOM/lessons/05.event-handling/scripts.js
@@ -1,6 +1,22 @@
 // 2. Property Method
 const btnTwo = document.querySelector("#btnTwo");
+const btnThree = document.querySelector("#btnThree");
 
 btnTwo.onclick = function () {
   alert("This alert is showing using Property Method");
 };
+
+// 3. using addEventListener()
+// we can add multiple event in single btn
+// we can remove event listener using removeEventListener('event', function)
+
+function random() {
+  console.log("Using event listener");
+}
+function doubleClick() {
+  console.log("Using event listener: by double click");
+}
+btnThree.addEventListener("click", random);
+btnThree.addEventListener("dblclick", doubleClick);
+
+btnThree.removeEventListener("dblclick", doubleClick);
