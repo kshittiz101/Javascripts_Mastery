2f4f1f66547e314ec9d2e7062cddd173be6545ab
kshittiz101 <chaudharykshittiz950@gmail.com>
2025-07-20 06:25:49 +0545

    data is fetch from api

--- intermediate/async_programming/todo/app.js
+++ intermediate/async_programming/todo/app.js
@@ -1,37 +1,21 @@
-// selecting input field and button
-
+// selecting add btn and input field
 let input = document.querySelector(".custom-input");
+let addBtn = document.querySelector("#add-btn");
+const APIURL = "https://687b030babb83744b7ee8555.mockapi.io/api/v1/todos";
+console.log(addBtn);
 console.log(input);
 
-let addBtn = document.querySelector("#add-btn");
-console.log(addBtn);
-
-// api url
-let apiUrl = "https://687b030babb83744b7ee8555.mockapi.io/api/v1/todos";
-
-// Event listener for Add Button
-addBtn.addEventListener("click", () => {
-  let newTask = input.value;
-  console.log(newTask);
-  console.log("clicked");
-
-  // Optionally: Clear input field after getting value
-  // input.value = "";
+addBtn.addEventListener("click", function () {
+  let newTodo = input.value;
+  console.log(newTodo);
+  console.log("Task is added");
+  input.value = "";
 });
 
-// Async function to fetch data from API
 async function fetchData() {
-  try {
-    let response = await fetch(apiUrl);
-    let data = await response.json();
-    console.log(data);
-
-    data.forEach((obj) => {
-      console.log(obj);
-    });
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
+  let response = await fetch(APIURL);
+  let data = response.json();
+  console.log(data);
 }
 
 fetchData();
